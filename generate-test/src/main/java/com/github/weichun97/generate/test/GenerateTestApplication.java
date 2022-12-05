fd37998548e61031ce28819996a99ce67d9ea57a
chun <1136389101@qq.com>
2022-12-05 23:10:51 +0800

    临时

--- generate-test/src/main/java/com/github/weichun97/generate/test/GenerateTestApplication.java
+++ generate-test/src/main/java/com/github/weichun97/generate/test/GenerateTestApplication.java
@@ -1,4 +1,4 @@
-package com.github.weichun97.generate.autoconfigure;
+package com.github.weichun97.generate.test;
 
 import com.github.weichun97.generate.autoconfigure.annotations.EnableGenerate;
 import org.springframework.boot.SpringApplication;
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableGenerate
 @SpringBootApplication
-public class GenerateAutoconfigureApplication {
+public class GenerateTestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GenerateAutoconfigureApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateTestApplication.class, args);
+	}
 
 }
---
+++ generate-test/src/test/java/com/github/weichun97/generate/test/GenerateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.weichun97.generate.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
