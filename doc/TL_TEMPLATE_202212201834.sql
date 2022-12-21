dd53616e6bb7eb591b1bbb9270f2c91de7a94444
chun <1136389101@qq.com>
2022-12-21 19:04:18 +0800

    update: 优化

--- doc/TL_TEMPLATE_202212201834.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO PUBLIC.TL_TEMPLATE (NAME,CREATE_TIME,UPDATE_TIME,DELETE_TIME,CUSTOM_FIELD,BASE_DIR) VALUES
-	 ('默认-java','2022-12-14 15:30:27.745+08','2022-12-20 18:06:41.14+08',NULL,'[{"key":"author","value":"chun"}]','com/itran/fgoc/server'),
-	 ('测试1','2022-12-15 18:20:58.468+08','2022-12-20 10:31:17.992+08',NULL,'[{"key":"a","value":"21"}]',NULL);
